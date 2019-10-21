9eabc8e9b38f7d1b5b5dd8c2e48c490ffd6ff0a0
dvitale199 <d.vitale199@gmail.com>
2019-10-21 16:35:36 -0600

    cleanup

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,14 +4,6 @@ import matplotlib.pyplot as plt
 import cv2
 import math
 import numpy as np
-
-
-import random
-from scipy import ndarray
-import skimage as sk
-from skimage import transform
-from skimage import util
-from skimage.viewer import ImageViewer
 import os
 
 # test to make sure plots are showing
@@ -24,17 +16,18 @@ import os
 path_dir_train = '/home/ubuntu/Deep-Learning/plant-classification/load_data/train/'
 
 image_path = path_dir_train + 'Asclepias tuberosa/Asclepias curassavica.94.jpg'
-
+#read image
 img = cv2.imread(image_path)
+#convert color bgr to rgb (test image read in with default bgr makes yellow flowers look blue)
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+# convert image to gray
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#convert image to hsv
 img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
+#edge detection on image
+edges = cv2.Canny(img,100,200)
 
 #thresholding
-#here 0 means that the image is loaded in gray scale format
-# gray_image = cv2.imread('index.png',0)
-
 ret,thresh_binary = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY)
 ret,thresh_binary_inv = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY_INV)
 ret,thresh_trunc = cv2.threshold(img_gray,127,255,cv2.THRESH_TRUNC)
@@ -42,27 +35,18 @@ ret,thresh_tozero = cv2.threshold(img_gray,127,255,cv2.THRESH_TOZERO)
 ret,thresh_tozero_inv = cv2.threshold(img_gray,127,255,cv2.THRESH_TOZERO_INV)
 
 
-
 # use the following to visualize the image augmentations
-names_list = ['oiriginal','gray','hsv','binary_thresh','binary_inv_thresh','trunc_thresh','tozero_thresh','tozero_inv_thresh']
-images_list = [img,img_gray,img_hsv,thresh_binary,thresh_binary_inv,thresh_trunc,thresh_tozero,thresh_tozero_inv]
+names_list = ['oiriginal','gray','hsv','edges','binary_thresh','binary_inv_thresh','trunc_thresh','tozero_thresh','tozero_inv_thresh']
+images_list = [img,img_gray,img_hsv,edges,thresh_binary,thresh_binary_inv,thresh_trunc,thresh_tozero,thresh_tozero_inv]
 
 def visualize_augmentations(names,images):
-    # fig, ax = plt.subplots(len(names)/3, 3, sharey=True, figsize=(15,15))
+
     for i in range(len(names)):
         plt.subplot(math.ceil(len(names)/3),3,i+1),plt.imshow(images[i],'gray')
         plt.title(names[i])
         plt.xticks([]),plt.yticks([])
         plt.show()
 
-# visualize_augmentations(names_list,images_list)
+visualize_augmentations(names_list,images_list)
 
 # cv2.imwrite('test_write.jpg',image)
-
-
-
-# edge detection
-edges = cv2.Canny(img,100,200)
-# plot edges
-plt.imshow(edges)
-plt.show()
